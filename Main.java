a01e7c28c1863e8a37e81a5aac6fdcd5e2c96d1f
Victoriashulman <victorias252@nycstudents.net>
2025-05-04 21:20:20 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
-
+public class Main{
+    public static void main(String[] args)
+    {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch g = new WordMatch("aaaabb");
+        System.out.println(g.scoreGuess("a"));
+        System.out.println(g.scoreGuess("aa"));
+        System.out.println(g.scoreGuess("aaa"));
+        System.out.println(g.scoreGuess("aabb"));
+        System.out.println(g.scoreGuess("c"));
+        WordMatch game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess ("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con" ));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+    }
+}
--- WordMatch.java
+++ WordMatch.java
@@ -1,1 +1,39 @@
+public class WordMatch {
+   
+    private String secret;
+
+
+    
+    public WordMatch(String word) {
+        secret = word;
+    }
+
+
+    
+    public int scoreGuess(String guess) {
+        int score = 0;
+        for(int i = 0; i <= secret.length() - guess.length(); i++)
+        {
+            if (secret.substring(i, i + guess.length()).equals(guess))
+                score ++;
+        }
+        return score * guess.length() * guess.length();
+    }
+     public String findBetterGuess(String guess1, String guess2) 
+     {
+       int score1 = scoreGuess(guess1);
+       int score2 = scoreGuess(guess2);
+
+       if(score1 > score2)
+        return guess1;
+       else if(score2 > score1)
+        return guess2;
+       else{
+            if(guess1.compareTo(guess2) > 0)
+                return guess1;
+            else
+                return guess2;
+       }
+     }
+}
 
